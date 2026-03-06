ef1b9e937aaf8cb10024883b8ef22fa1d58daf23
Nikadro1 <nico_benoit@hotmail.com.ar>
2026-03-06 18:09:51 -0300

    error en linea del cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-};));
+}));
 app.options("*", cors());
 app.use(express.json());
 app.use(rutas);
